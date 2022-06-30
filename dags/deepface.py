919d98ad80de21e44d3415db59847417d17501e9
EugeniaFranchi <mfranchi@fi.uba.ar>
2022-06-30 12:57:45 -0300

    compara por mail

--- dags/deepface.py
+++ dags/deepface.py
@@ -1,23 +1,16 @@
-
-from asyncio import tasks
-from datetime import datetime, timedelta
-from textwrap import dedent
-
 from airflow import DAG
 from airflow.operators.python import PythonOperator
+from airflow.models import Variable
 
+from datetime import datetime, timedelta
 from pymongo import MongoClient
 from deepface import DeepFace
-import base64
 
-#def _get_attendance():
-#    client = MongoClient('mongodb://euge:1234@cluster0-shard-00-00.b2f7j.mongodb.net:27017,cluster0-shard-00-01.b2f7j.mongodb.net:27017,cluster0-shard-00-02.b2f7j.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-dw8ukd-shard-0&authSource=admin&retryWrites=true&w=majority')
-#    db = client["myFirstDatabase"]
-#    col = db["attendance"]
-#    return list(col.find())
+PRESENT = 'presente'
+ABSENT = 'ausente'
 
 def _validate(ti):
-    client = MongoClient('mongodb://euge:1234@cluster0-shard-00-00.b2f7j.mongodb.net:27017,cluster0-shard-00-01.b2f7j.mongodb.net:27017,cluster0-shard-00-02.b2f7j.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-dw8ukd-shard-0&authSource=admin&retryWrites=true&w=majority')
+    client = MongoClient(Variable.get("MONGO_URL"))
     db = client["myFirstDatabase"]
     col_attendance = db["attendance"]
     col_students = db["students"]
@@ -25,24 +18,24 @@ def _validate(ti):
     
     for attendee in attendees:
         course = attendee['course']
-        students = col_students.find({'course': course, 'state':{'$exists':False}})
+        email = attendee['email']
+        student = col_students.find_one({'email':email, 'course':course})
         
-        for student in students:
-            result = DeepFace.verify([[attendee['photo'], student['photo']]],
-                                    model_name = "VGG-Face",
-                                    distance_metric = "cosine",
-                                    detector_backend = "opencv"
-            )
-            if result["pair_1"]["verified"]:
-                col_students.update_one({'_id':student['_id']}, {"$set":{'state':'presente'}})
-                break
+        result = DeepFace.verify([[attendee['photo'], student['photo']]],
+                                model_name = "VGG-Face",
+                                distance_metric = "cosine",
+                                detector_backend = "opencv"
+        )
+
+        if result["pair_1"]["verified"]:
+            col_students.update_one({'_id':student['_id']}, {"$set":{'state':PRESENT}})
 
 def _complete_attendance():
-    client = MongoClient('mongodb://euge:1234@cluster0-shard-00-00.b2f7j.mongodb.net:27017,cluster0-shard-00-01.b2f7j.mongodb.net:27017,cluster0-shard-00-02.b2f7j.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-dw8ukd-shard-0&authSource=admin&retryWrites=true&w=majority')
+    client = MongoClient(Variable.get("MONGO_URL"))
     db = client["myFirstDatabase"]
     col_students = db["students"]
     for student in col_students.find({'state':{'$exists':False}}):
-        col_students.update_one({'_id':student['_id']}, {"$set":{'state':'ausente'}})
+        col_students.update_one({'_id':student['_id']}, {"$set":{'state':ABSENT}})
     return 'verified'
     
 
